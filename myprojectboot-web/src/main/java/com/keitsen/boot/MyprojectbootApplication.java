51d57841dcf8337b692465dec45443d1c2b494d6
miles <vigogiv@hotmail.com>
2019-11-08 16:03:55 +0800

    first commit

---
+++ myprojectboot-web/src/main/java/com/keitsen/boot/MyprojectbootApplication.java
@@ -0,0 +1,13 @@
+package com.keitsen.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectbootApplication.class, args);
+	}
+
+}
---
+++ myprojectboot-web/src/test/java/com/keitsen/boot/MyprojectbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keitsen.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
